773c461a45b385bab01a5ee1cd1ef5d449600e71
hochan Lee <hochan049@gmail.com>
2021-04-10 21:43:05 +0900

    [FEAT] add two host

--- test.js
+++ test.js
@@ -1,9 +1,26 @@
 const request = require("supertest");
 
-describe("Test the root path", () => {
+// describe("Test the root path", () => {
+// 	test("It should response the GET method", done => {
+// 		request("http://localhost:8080")
+// 			.get("/")
+// 			.then(res => {
+// 				console.log(res.status);
+// 				console.log(res.headers);
+// 				console.log(res.text);
+// 				expect(res.statusCode).toBe(200);
+// 				done();
+// 			});
+// 	});
+// });
+
+describe("Test two host", () => {
 	test("It should response the GET method", done => {
 		request("http://localhost:8080")
 			.get("/")
+			.set('Host', 'ftnix')
+			.set('Host', 'Ftnix')
+			.set('host', 'Ftnix')
 			.then(res => {
 				console.log(res.status);
 				console.log(res.headers);
@@ -13,3 +30,4 @@ describe("Test the root path", () => {
 			});
 	});
 });
+
